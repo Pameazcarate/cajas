29f2233d712b33bfedd9001a40b4eafa11911ff7
Pameazcarate <pamela.azcarate@cua.uam.mx>
2025-03-28 09:20:41 -0600

    first commit

---
+++ src/main/java/com/example/integra/cajas/CajasApplication.java
@@ -0,0 +1,13 @@
+package com.example.integra.cajas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CajasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/integra/cajas/CajasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.integra.cajas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
